6f19b8d60242b147bf4851cbf943f8519ebd675f
pabuzak <pabuzak@gmail.com>
2024-07-03 19:41:35 +1200

    Methods

---
+++ Method.java
@@ -0,0 +1,25 @@
+// attributes
+class Computer{
+    public void playMusic(){
+        System.out.println("Music Playing...");
+    }
+
+    public String getMeAPen(int cost){
+        if(cost>=10)
+            return "Pen";
+        
+        return "Nothing";
+    }
+}
+
+public class Method{
+    public static void main(String args[]){
+        Computer obj = new Computer();
+
+        obj.playMusic();
+
+        String str = obj.getMeAPen(12);
+        System.out.println(str);
+        
+    }
+}

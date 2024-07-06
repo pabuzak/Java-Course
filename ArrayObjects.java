2788975db205c81322e09b2d3ce762df6a36030c
pabuzak <pabuzak@gmail.com>
2024-07-06 23:34:31 +1200

    Advanced loop

--- ArrayObjects.java
+++ ArrayObjects.java
@@ -30,19 +30,27 @@ public class ArrayObjects{
         students[1] = s2;
         students[2] = s3;
 
-        for(int i=0;i<students.length;i++){
-            System.out.println(students[i].name + " : " + students[i].marks);
+        // for(int i=0;i<students.length;i++){
+        //     System.out.println(students[i].name + " : " + students[i].marks);
+        // }
+
+        for(Student stud : students){
+            System.out.println(stud.name + " : " + stud.marks);
         }
 
-        // int nums[] = new int[6];
+        // int nums[] = new int[4];
 
         // nums[0] = 4;
         // nums[1] = 8;
-        // nums[2] = 9;
+        // nums[2] = 3;
         // nums[3] = 9;
 
         // for(int i=0;i<nums.length;i++){
         //     System.out.println(nums[i])
+        // }
+
+        // for(int n : nums){  // for each loop
+        //     System.out.println(n);
         // }
 
     }
---
+++ Strings.java
@@ -0,0 +1,10 @@
+
+
+public class Strings{
+    public static void main(String args[]){
+        String name = new String();  // string is an actual class
+
+        System.out.println(name);
+
+    }
+}

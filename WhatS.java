1488388a7e6c140c5f0e62dc07c72ff7c4eb8621
pabuzak <pabuzak@gmail.com>
2024-07-08 15:56:28 +1200

    String is a class

---
+++ WhatS.java
@@ -0,0 +1,9 @@
+public class WhatS{
+    public static void main(String args[]){
+        String name = new String("navin");
+        name = "reddy " + name;
+
+        System.out.println("hello " + name);
+    }
+
+}

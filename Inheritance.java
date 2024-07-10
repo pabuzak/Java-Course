fc5877aac35efd1fa931b30125186a0b42f9c9c2
pabuzak <pabuzak@gmail.com>
2024-07-10 20:05:57 +1200

    Inheritance

---
+++ Inheritance.java
@@ -0,0 +1,13 @@
+
+
+public class Inheritance{
+    public static void main(String[] args) {
+        AdvCalc obj = new AdvCalc();
+        int r1 = obj.add(4, 5);
+        int r2 = obj.sub(7, 3);
+        int r3 = obj.multi(5, 3);
+        int r4 = obj.div(15, 4);
+
+        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
+    }
+}

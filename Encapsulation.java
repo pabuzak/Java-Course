e283784633cc1eb23ec07ed556578d56ad0afdfc
pabuzak <pabuzak@gmail.com>
2024-07-10 12:08:46 +1200

    Constructors

--- Encapsulation.java
+++ Encapsulation.java
@@ -1,12 +1,30 @@
 class Human{
     private int age; // only acessbile in the same class. No one in the outside world can use it.
     private String name; // every time you make an instance variable, make it private.
-    
+
+    // constructor looks like a method itself.
+    // its use as a 'default value' for the variables.
+    public Human(){ // default constructor
+        age = 12;
+        name = "John";
+    }
+
+    public Human(int a, String n){  // Parameterized contructor
+        age = a;
+        name = n;
+    }
+
+    public Human(String n){  // Parameterized contructor
+        this.age = 12;
+        name = n;
+    }
+
     public int getAge() {
         return age;
     }
     public void setAge(int age) {
-        this.age = age;
+        this.age = age; // this means it indicates it is an instance variable, representing the current object
+
     }
     public String getName() {
         return name;
@@ -14,8 +32,6 @@ class Human{
     public void setName(String name) {
         this.name = name;
     }
-
-    
 
     // public int getAge(){
     //     return age;
@@ -37,13 +53,17 @@ class Human{
 public class Encapsulation{
     public static void main(String args[]){
         Human obj = new Human();
+        Human obj1 = new Human(18, "Navin");
+        System.out.println(obj.getName() + " : " + obj.getAge());
+        System.out.println(obj1.getName() + " : " + obj1.getAge());
 
-        obj.setAge(30);
-        obj.setName("Navin");
+        // obj.setAge(30);
+        // obj.setName("Navin");
 
-        int age = obj.getAge();
-        String name = obj.getName();
+        // int age = obj.getAge();
+        // String name = obj.getName();
 
-        System.out.println(age + " : " + name);
+        // System.out.println(age + " : " + name);
+        // System.out.println(obj1.getName() + " : " + obj.getAge());
     }
 }

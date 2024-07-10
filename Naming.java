304f516447415e76cdcd8f6a12b15955200187c1
pabuzak <pabuzak@gmail.com>
2024-07-10 19:36:29 +1200

    Anonymous Object

---
+++ Naming.java
@@ -0,0 +1,36 @@
+class A{
+    public A(){
+        System.out.println("object created");
+    }
+
+    public void show(){
+        System.out.println("in A show");
+    }
+}
+
+public class Naming {
+    public static void main(String[] args) {
+        // int marks;
+        // marks = 99;
+
+        // A obj; // reference creation
+        // obj = new A();
+        // obj.show();
+
+        new A().show(); // anonymous object
+
+    }   
+}
+
+
+// Camel casing
+
+// class and interace - Calc, Runable
+// variable and method - marks, show()
+// constants - PIE, BRAND
+
+
+// showMyMarks()
+// MyData - class
+// Human() - contructor
+
